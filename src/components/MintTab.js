96b9fd85f5aa0cbdc60bafc5cf7785872e0a2c1e
dshank-forge <j.david.shank@gmail.com>
2022-03-16 22:30:14 -0400

    started working on the mint button

--- src/components/MintTab.js
+++ src/components/MintTab.js
@@ -1,3 +1,23 @@
+// import {useEthers, useEtherBalance} from '@usedapp/core';
+import {Button, Card} from "@mui/material";
+import "./Components.css";
+import essay from "../images/essay.jpg";
+
+
+// Mint Button
+// 1. Disabled if metamask is not connected 
+// 2. Enabled if metamask is connected 
+
+
 export const MintTab = () => {
-   return( <div>Mint</div> )
+   return (
+   <div>
+      <Button variant="contained" color="secondary" size="large">
+         Mint
+      </Button>
+      <Card>
+         <img src={essay}></img>
+      </Card>
+   </div>
+   )
 }
