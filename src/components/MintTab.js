cce8027a677b8573106381331749d85bfb09b870
dshank-forge <j.david.shank@gmail.com>
2022-03-17 22:04:53 -0400

    Added a mint button that becomes enable or disabled based on whether the user is connected.

--- src/components/MintTab.js
+++ src/components/MintTab.js
@@ -1,23 +1,35 @@
-// import {useEthers, useEtherBalance} from '@usedapp/core';
+import {useEthers} from '@usedapp/core';
 import {Button, Card} from "@mui/material";
 import "./Components.css";
 import essay from "../images/essay.jpg";
 
 
-// Mint Button
-// 1. Disabled if metamask is not connected 
-// 2. Enabled if metamask is connected 
+// NFT thumbnails / art
+// 1. Create a flex box to contain all 8 pieces
+// 2. A flex box tries to fit everything on one line, have it wrap the pieces onto multiple lines if needed 
+// 3. Use horizontal as the main axis and vertical as the cross axis 
 
 
 export const MintTab = () => {
+   const {account, activateBrowserWallet, deactivate} = useEthers()
+
+   const isConnected = account !== undefined
+
    return (
-   <div>
-      <Button variant="contained" color="secondary" size="large">
-         Mint
-      </Button>
-      <Card>
-         <img src={essay}></img>
-      </Card>
-   </div>
+      <div>
+         { isConnected ?
+            <Button variant="contained" color="secondary" size="large" 
+               onClick={ () => console.log('minted') }>
+               Mint
+            </Button>
+            :
+            <Button variant="contained" color="secondary" size="large" disabled>
+               Mint
+            </Button>
+         }
+         <Card>
+            <img src={essay}></img>
+         </Card>
+      </div>
    )
 }
