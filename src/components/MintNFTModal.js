2f6a773d40f44f423661e093aa4f1ae8c0e219d0
dshank-forge <j.david.shank@gmail.com>
2022-04-03 13:45:46 -0400

    Added props to the NFT Modal so that its state comes from the parent component, while still allowing the modal itself to change that state.

--- src/components/MintNFTModal.js
+++ src/components/MintNFTModal.js
@@ -1,12 +1,44 @@
 import "./Components.css";
+import essay from "../images/essay.jpg";
+import hand from "../images/by_my_own_hand.jpg";
+import twister from "../images/WhirlwindGIF.gif";
+import covid from "../images/Covid-ThoughtsGIF.gif";
+import { useState } from "react";
 
-// We want this component to be rendered when the user clicks on an individual NFT
-// in the Mint tab
+export const MintNFTModal = ( mintNFTProps ) => {
+    
+    const pictures = {
+        0: essay, 
+        1: covid, 
+        2: twister, 
+        3: hand, 
+        4: essay, 
+        5: essay, 
+        6: essay, 
+        7: essay 
+    }
 
-export const MintNFTModal = () => {
+    console.log(mintNFTProps)
+
+    const selectedNFTindex = mintNFTProps['index']
+    const selectedNFTImage = pictures[selectedNFTindex]
+
+    const modalEnabled = mintNFTProps['modalEnabled']
+    const setModalEnabled = mintNFTProps['setModalEnabled']
+    
+
+    const handleExit = () => {
+        setModalEnabled(false)    
+    }
+    
     return (
-        <div className="mintNFTModal">
-            Single NFT
-        </div>
+        <> 
+            { modalEnabled  &&
+                <div className="mintNFTModal">
+                    <img src={selectedNFTImage} className="largeImage"></img>
+                    <button onClick={handleExit}>Exit</button>
+                </div>
+            }
+        </>
     )
 }
--- src/components/MintTab.js
+++ src/components/MintTab.js
@@ -8,25 +8,22 @@ import hand from "../images/by_my_own_hand.jpg";
 import twister from "../images/WhirlwindGIF.gif";
 import covid from "../images/Covid-ThoughtsGIF.gif";
 
-const pictures = {
-   0: essay, 
-   1: covid, 
-   2: twister, 
-   3: hand, 
-   4: essay, 
-   5: essay, 
-   6: essay, 
-   7: essay 
-}
-console.log(pictures)
-
-const o = new Object()
-
 export const MintTab = () => {
+
+   const pictures = {
+      0: essay, 
+      1: covid, 
+      2: twister, 
+      3: hand, 
+      4: essay, 
+      5: essay, 
+      6: essay, 
+      7: essay 
+   }
 
    const [imageClass, setImageClass] = useState("smallImage")   
    const [modalEnabled, setModalEnabled] = useState(false)
-   const [selectedNFT, setSelectedNFT] = useState(0)
+   const [selectedNFTIndex, setSelectedNFTIndex] = useState(0)
 
    const enlargeImage = (e) => {
       const image = e.target
@@ -38,14 +35,14 @@ export const MintTab = () => {
       image.className = "smallImage"
    }
 
-   const toggleModal = () => {
-      // console.log(e.target.id)
-      setModalEnabled(!modalEnabled)
-   }
+   // const toggleModal = () => {
+   //    // console.log(e.target.id)
+   //    setModalEnabled(!modalEnabled)
+   // }
 
    const handleNFTClick = (e) => {
-      setSelectedNFT(e.target.id)
-      toggleModal()
+      setSelectedNFTIndex(e.target.id)
+      setModalEnabled(true)
    }
 
    const {account, activateBrowserWallet, deactivate} = useEthers()
@@ -68,20 +65,20 @@ export const MintTab = () => {
             </Button>
          }
 
-         { modalEnabled && <MintNFTModal />  }
+         <MintNFTModal index={selectedNFTIndex} modalEnabled={modalEnabled} setModalEnabled={setModalEnabled}/>
 
          <div className='nftsContainer'>
             { 
-               // console.log(pictures.entries())
-                  // <div>
-                  //    <img src={entry[1]} id={entry[0]}
-                  //       className="smallImage" 
-                  //       onMouseOver={enlargeImage} 
-                  //       onMouseOut={shrinkImage}
-                  //       onClick={handleNFTClick}>
-                  //    </img>
-                  // </div>
-               
+               Object.entries(pictures).map( (entry) => 
+                  <div key={entry[0]}>
+                     <img id={entry[0]} src={entry[1]}
+                        className="smallImage" 
+                        onMouseOver={enlargeImage} 
+                        onMouseOut={shrinkImage}
+                        onClick={handleNFTClick}>
+                     </img>
+                  </div>
+               )
             }
          </div>
       </div>
